691673b2cc9bef4ee0a4b34f4cf6ba910d7ae55c
Robert Thurnher <robert@thurnher.email>
2017-07-15 21:07:41 +0200

    Add some testing

--- src/test/kotlin/net/robi42/gisink/GiSinkApplicationTests.kt
+++ src/test/kotlin/net/robi42/gisink/GiSinkApplicationTests.kt
@@ -1,18 +1,28 @@
 package net.robi42.gisink
 
-import org.junit.Ignore
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.cloud.stream.annotation.Bindings
+import org.springframework.cloud.stream.messaging.Sink
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
+import javax.inject.Inject
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner::class)
 @SpringBootTest class GiSinkApplicationTests {
 
-    @Ignore("Requires running Redis/Kafka")
+    @Inject
+    @Bindings(LoggingSink::class)
+    @Suppress("SpringKotlinAutowiring")
+    private lateinit var sink: Sink
+
     @Test fun `context loads`() {}
+
+    @Test fun `wires up sink input channel`() {
+        assertThat(sink.input()).isNotNull()
+    }
 
 }
